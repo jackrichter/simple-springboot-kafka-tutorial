24e2b829a83217fbb26b83106475daa156a9a884
jackrichter <jackrichter1@gmail.com>
2023-03-01 01:58:10 +0100

    First commit of a real basic springboot-kafka project.

---
+++ src/main/java/net/javaguides/springboot/kafka/KafkaProducer.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    // In Spring Boot, the Spring Container does all configuration of KafkaTemplate class
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    /**
+     * No need for @Autowired because of only one parameterized Constructor.
+     * @param kafkaTemplate
+     */
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOGGER.info(String.format("Message sent: %s", message));
+        kafkaTemplate.send(topicName, message);
+    }
+}
---
+++ src/main/java/net/javaguides/springboot/payload/User.java
@@ -0,0 +1,43 @@
+package net.javaguides.springboot.payload;
+
+/**
+ * Simple POJO to serve as a JSON Serializable and De-serializable payload to Produce and Consume using Kafka.
+ */
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
